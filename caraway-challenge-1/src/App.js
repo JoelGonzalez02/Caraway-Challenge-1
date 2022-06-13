ebc0272a14dfa0aeea3b28cda525d063c60c3a86
joelgonzadev <joel@alliofintech.io>
2022-06-12 20:25:48 -0500

    First draft

--- caraway-challenge-1/src/App.js
+++ caraway-challenge-1/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #c3f63b;
+`;
+
+const Button = styled.button`
+  height: 70px;
+  width: 330px;
+  border-radius: 50px;
+  background-color: #353b48;
+  color: #c3f63b;
+  font-size: 30px;
+  font-family: "Open Sans", sans-serif;
+  border: none;
+  box-shadow: 14px 14px white;
+  transition: 0.2s ease-in;
+  &:hover {
+    background-color: #f5f6fa;
+    color: black;
+    box-shadow: -14px -14px #353b48;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button>ADD TO CART</Button>
+    </Container>
   );
 }
 
