eb3081b40728e159c80427fe306bc626145a18b9
joelgonzadev <joel@alliofintech.io>
2022-06-15 13:21:23 -0500

    Final

--- caraway-challenge-1/src/App.js
+++ caraway-challenge-1/src/App.js
@@ -1,32 +1,32 @@
 import "./App.css";
 import styled from "@emotion/styled";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: #c3f63b;
-`;
+const Container = styled.div(() => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  width: "100vw",
+  backgroundColor: "#c3f63b",
+}));
 
-const Button = styled.button`
-  height: 70px;
-  width: 330px;
-  border-radius: 50px;
-  background-color: #353b48;
-  color: #c3f63b;
-  font-size: 30px;
-  font-family: "Open Sans", sans-serif;
-  border: none;
-  box-shadow: 14px 14px white;
-  transition: 0.2s ease-in;
-  &:hover {
-    background-color: #f5f6fa;
-    color: black;
-    box-shadow: -14px -14px #353b48;
-  }
-`;
+const Button = styled.button(() => ({
+  width: "330px",
+  height: "70px",
+  borderRadius: "50px",
+  backgroundColor: "#353b48",
+  color: "#c3f63b",
+  fontSize: "30px",
+  fontFamily: "Open Sans, sans-serif",
+  border: "none",
+  boxShadow: "14px 14px white",
+  transition: "0.2s ease-in",
+  "&:hover": {
+    backgroundColor: "#f5f6fa",
+    color: "black",
+    boxShadow: "-14px -14px #353b48",
+  },
+}));
 
 function App() {
   return (
